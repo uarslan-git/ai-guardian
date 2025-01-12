e311ed680471ca313c2f1e9cf45293f44406b58e
Daniel Lucht <daniel.lucht@uni-bielefeld.de>
2025-01-12 20:20:35 +0100

    Better scatter plots. Added noise to data. SVC statt MLPClassifier als model für over-/underfitting

--- backend/scatter_boundary.py
+++ backend/scatter_boundary.py
@@ -1,23 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import make_moons, make_classification, make_multilabel_classification
+from sklearn.datasets import make_moons, make_classification, make_multilabel_classification, make_blobs
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
 
 
 import pandas as pd
 
 # Generate a synthetic dataset
 #X, y = make_moons(n_samples=1000, noise=0.3, random_state=42)
-# X, y = make_classification(n_samples=1000, n_classes=3, n_features=2, n_informative=2, n_clusters_per_class=1, n_redundant=0, random_state=42)
+# X, y = make_classification(n_samples=1000, n_features=2, n_clusters_per_class=1, n_redundant=0, random_state=40)
+# X, y = make_blobs(n_samples=1000, n_features=2, centers=2, random_state=42)
 # For more: https://scikit-learn.org/stable/datasets/sample_generators.html#generators-for-classification-and-clustering
-
 
 # Use Spotify Dataset
 data = pd.read_csv("../data/spotify_tracks.csv")
+# Feature Names
+# track_id,track_name,artist_name,year,popularity,artwork_url,album_name,acousticness,danceability,
+# duration_ms,energy,instrumentalness,key,liveness,loudness,mode,speechiness,tempo,time_signature,
+# valence,track_url,language
 complete_X = data[['popularity', 'danceability']]
-
 # X[0] := popularity
 # X[1] := danceability
 
@@ -26,14 +30,20 @@ X_popular = X[:, 0]
 X_dance = X[:, 1]
 
 y = np.zeros(shape=X.shape[0])
-#y[:int(y.shape[0]/2)] = 1
-y[np.logical_or(X_popular > 50, X_dance > 0.7)] = 1
+# y[:int(y.shape[0]/2)] = 1
+# y[np.logical_or(X_popular > 50, np.logical_and(X_dance > 0.5, X_dance < 0.8))] = 1
+y[np.logical_or(X_popular > 50, X_dance > 0.6)] = 1
+# y[X_popular > 50] = 1
+
+# Apply noise
+print(f'Num likes:{int(np.sum(y))} / {y.shape[0]}')
+noise_idx = np.random.choice(len(y), size=int(0.1*len(y)), replace=False)
+y[noise_idx] = 1 - y[noise_idx]
 print(f'Num likes:{int(np.sum(y))} / {y.shape[0]}')
 
 
-
 # Split into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
 # Function to plot decision boundary
 def plot_decision_boundary(model, X, y, title):
@@ -60,38 +70,48 @@ def plot_scatter_with_boundary(layer_size, X_train, X_test, y_train, y_test, tit
   #plt.subplot(1, 2, 2)
   plot_decision_boundary(mlp, X_test, y_test, title)
 
-layer_sizes=[5,100,1000]
-max_iter = 1000
+layer_sizes=[2,8,5000]
+max_iter = 2000
+
+# m_underfit = MLPClassifier(hidden_layer_sizes=(layer_sizes[0],), max_iter=max_iter)
+m_underfit = SVC(kernel='rbf', C=1, random_state=42)
+# m_optimal = MLPClassifier(hidden_layer_sizes=(layer_sizes[1],), max_iter=max_iter)
+m_optimal = SVC(kernel='rbf', C=10000, random_state=42)
+# m_overfit = MLPClassifier(hidden_layer_sizes=(layer_sizes[2],), max_iter=max_iter)
+m_overfit = SVC(kernel='rbf', C=1000000, random_state=42)
+
 
 # Underfitting example: Shallow network
-mlp_underfit = MLPClassifier(hidden_layer_sizes=(layer_sizes[0],), max_iter=max_iter)
-mlp_underfit.fit(X_train, y_train)
-y_pred_underfit = mlp_underfit.predict(X_test)
+m_underfit.fit(X_train, y_train)
+y_pred_underfit = m_underfit.predict(X_test)
 print(f'Underfitting Accuracy: {accuracy_score(y_test, y_pred_underfit)}')
 # plt.figure(figsize=(12, 5))
-#plt.subplot(3, 1, 1)
-plot_decision_boundary(mlp_underfit, X_test, y_test, 'Underfitted')
+plt.subplot(1, 2, 1)
+plot_decision_boundary(m_underfit, X_train, y_train, 'Underfitted Train')
+plt.subplot(1, 2, 2)
+plot_decision_boundary(m_underfit, X_test, y_test, 'Underfitted Test')
 plt.show()
 
 
-mlp_optimal = MLPClassifier(hidden_layer_sizes=(layer_sizes[1],), max_iter=max_iter)
-mlp_optimal.fit(X_train, y_train)
-y_pred_optimal = mlp_optimal.predict(X_test)
-print(f'Accuracy: {accuracy_score(y_test, y_pred_optimal)}')
-# plt.figure(figsize=(12, 5))
-#plt.subplot(3, 1, 2)
-plot_decision_boundary(mlp_optimal, X_test, y_test, 'Middle')
+m_optimal.fit(X_train, y_train)
+y_pred_optimal = m_optimal.predict(X_test)
+print(f'Middle Accuracy: {accuracy_score(y_test, y_pred_optimal)}')
+plt.subplot(1, 2, 1)
+plot_decision_boundary(m_optimal, X_train, y_train, 'Middle Train')
+plt.subplot(1, 2, 2)
+plot_decision_boundary(m_optimal, X_test, y_test, 'Middle Test')
 plt.show()
 
 
 # Overfitting example: Deep network with too many neurons
-mlp_overfit = MLPClassifier(hidden_layer_sizes=(layer_sizes[2],), max_iter=max_iter)
-mlp_overfit.fit(X_train, y_train)
-y_pred_overfit = mlp_overfit.predict(X_test)
+m_overfit.fit(X_train, y_train)
+y_pred_overfit = m_overfit.predict(X_test)
 print(f'Overfitting Accuracy: {accuracy_score(y_test, y_pred_overfit)}')
-#plt.figure(figsize=(12, 5))
-#plt.subplot(3, 1, 3)
-plot_decision_boundary(mlp_overfit, X_test, y_test, 'Overfitted')
+plt.subplot(1, 2, 1)
+plot_decision_boundary(m_overfit, X_train, y_train, 'Overfitted Train')
+plt.subplot(1, 2, 2)
+plot_decision_boundary(m_overfit, X_test, y_test, 'Overfitted Test')
+plt.show()
 
 # plot_scatter_with_boundary(5, X_train, X_test, y_train, y_test)
 # plt.show()
@@ -100,5 +120,5 @@ plot_decision_boundary(mlp_overfit, X_test, y_test, 'Overfitted')
 # plot_scatter_with_boundary(1000, X_train, X_test, y_train, y_test)
 
 
-plt.show()
+#plt.show()
 
