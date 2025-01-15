a74ae9fd9896cd7a2fec2794d28075cccb6306c5
Daniel Lucht <daniel.lucht@uni-bielefeld.de>
2025-01-15 17:34:08 +0100

    Provide plot endpoint with static plotly figure.

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask_cors import CORS
+import pickle
+import os
 
 app = Flask(__name__)
-CORS(app)
+
+def root_dir():
+    return os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/api/hello')
 def hello():
     return {'message': 'hello world from the backend'}
+
+@app.route('/api/get_plot/<path:path>')
+def get_plot(path):
+    fig = pickle.load(open(os.path.join(root_dir(), path), 'rb'))
+    return fig.to_json()
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True) 
