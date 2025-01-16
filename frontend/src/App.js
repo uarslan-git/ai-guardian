fb86312512743d00915e0604d0dd0f92ee0c5a38
Umut Arslan <umut_arslan@gmx.de>
2025-01-16 18:54:10 +0100

    Remove .venv folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <Welcome />
       <PokemonList />
+      <iframe class="Plot" src="http://127.0.0.1:5000/api/get_plot_html/assets/testplot.pkl"></iframe>
     </div>
   );
 }
--- frontend/src/Welcome.js
+++ frontend/src/Welcome.js
@@ -12,7 +12,23 @@ function Welcome() {
                 <h1>Ai-Guardian Assignment</h1>
                 <h2> A beginners Guide To Underfitting/Overfitting</h2>
                 <p>
-                    This is a simple React application designed to help you get started with building amazing web applications.
+                    Stell dir mal vor du würdest nicht wissen, was ein Hund und eine Katze sind. Nun zeigt man dir ein Beispielbild von beidem.
+                    Vermutlich würdest du sofort die Unterschiede erkennen können. Zeigt man dir nun Bilder von anderen Hunden, weißt du schon,
+                    dass das keine Katzen sind und könntest dutzende von Gründen aufzählen wieso.
+
+                    Maschinen fällt das schwieriger. Die Regeln werden sich anhand der Beispiele, die man ihr gibt erschlossen.
+                    Dabei können die Regeln unterschiedlich allgemein sein.
+                    Wenn die Regeln zu speziell sind redet man on Overfitting, wenn sie zu allgemein gehalten sind, redet man von underfitting.
+
+                    In der Realität kannst du dir das so vorstellen:
+                    Ein Modell, das overfittet ist, hat anhand der Daten gelernt, dass jedes Bild, wo ein vierbeiniges Tier auf dem Rasen
+                    mit einem Tennisball und einer Frisbey zu sehen ist, muss ein Hund sein. Wenn man nun aber ein Bild eines Huskys zeigt, wäre
+                    das Modell verwirrt, weil es seine Regeln gar nicht richtig anwenden kann.
+
+                    Umgekehrt können Regeln aber auch viel zu allgemein und damit das Modell underfittet sein.
+                    Die Regel könnte hier lauten, dass alles ein Hund ist, was 4 Beine hat.
+                    Diese Regel ist offensichtlich nicht sonderlich gut, denn auch Katzen haben meistens
+                    4 Beine.
                 </p>
             </header>
         </motion.div>
